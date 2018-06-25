02120511bc59bdb90b88cc46c8f1e1f6b7e21c22
WB591 <33439132+WB591@users.noreply.github.com>
2018-06-25 09:49:13 -0400

    Update quote.js

--- quote.js
+++ quote.js
@@ -1,17 +1,24 @@
+  var randomQuote;
+  var author;
+
 $(document).ready(function(){
   getQuote();
+});
 
 function getQuote(){
-
-  var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
-  $.getJSON(url, function(data){
-    $("#quote").html('"'+data.quoteText+'"'); 
-    $("#author").html("-"+data.quoteAuthor);
-  });
- 
+    var url= "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+    $.getJSON(url, function(data){
+      randomQuote = data.quoteText;
+      author = data.quoteAuthor;
+    $("#quote").html('"'+randomQuote+'"'); 
+    $("#author").html("-"+author);
+  }); 
 };
 
-    $("#newQuote").on("click", function(){
+$("#tweet").on("click", function(){
+      window.open("https://twitter.com/intent/tweet?text=" + randomQuote);
+  });
+
+$("#newQuote").on("click", function(){
     getQuote();
   });
-});
